898c9ef1c8fd81a2440931becf8d534648262a24
akaaariii <caaari23@gmail.com>
2021-03-09 00:52:18 -0800

    added project image

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { BgPrimary, Wrapper } from '../../globalStyles'
 import { WorksContainer, WorkWrapper, OverlayText } from './styles'
+import work1Img from '../../assets/images/work_restaurant.png'
+import work2Img from '../../assets/images/work_recipe_app.png'
 
 const Projects = () => {
   return (
@@ -10,10 +12,20 @@ const Projects = () => {
 
         <WorksContainer>
           <WorkWrapper>
-            <img src="https://via.placeholder.com/300x300" alt=""/>
+            <img src={work1Img} alt="Restaurant website" />
             <OverlayText>
               <div>Title</div>
-              <div>Description : Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, iure deleniti dolorem fuga praesentium perspiciatis adipisci aliquam ipsa saepe laboriosam. Repellat amet numquam saepe aut voluptates perferendis temporibus</div>
+              <div>Description : Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, iure deleniti dolorem fuga praesentium perspiciatis adipisci aliquam ipsa saepe laboriosam.</div>
+              <button>Visit Website</button>
+              <button>Code</button>
+            </OverlayText>
+          </WorkWrapper>
+  
+          <WorkWrapper>
+            <img src={work2Img} alt="Recipe app"/>
+            <OverlayText>
+              <div>Title</div>
+              <div>Description : Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, iure deleniti dolorem fuga praesentium perspiciatis adipisci aliquam ipsa saepe laboriosam.</div>
             </OverlayText>
           </WorkWrapper>
   
@@ -21,7 +33,7 @@ const Projects = () => {
             <img src="https://via.placeholder.com/300x300" alt=""/>
             <OverlayText>
               <div>Title</div>
-              <div>Description : Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, iure deleniti dolorem fuga praesentium perspiciatis adipisci aliquam ipsa saepe laboriosam. Repellat amet numquam saepe aut voluptates perferendis temporibus</div>
+              <div>Description : Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, iure deleniti dolorem fuga praesentium perspiciatis adipisci aliquam ipsa saepe laboriosam.</div>
             </OverlayText>
           </WorkWrapper>
   
@@ -29,15 +41,23 @@ const Projects = () => {
             <img src="https://via.placeholder.com/300x300" alt=""/>
             <OverlayText>
               <div>Title</div>
-              <div>Description : Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, iure deleniti dolorem fuga praesentium perspiciatis adipisci aliquam ipsa saepe laboriosam. Repellat amet numquam saepe aut voluptates perferendis temporibus</div>
+              <div>Description : Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, iure deleniti dolorem fuga praesentium perspiciatis adipisci aliquam ipsa saepe laboriosam.</div>
             </OverlayText>
           </WorkWrapper>
-  
+
           <WorkWrapper>
             <img src="https://via.placeholder.com/300x300" alt=""/>
             <OverlayText>
               <div>Title</div>
-              <div>Description : Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, iure deleniti dolorem fuga praesentium perspiciatis adipisci aliquam ipsa saepe laboriosam. Repellat amet numquam saepe aut voluptates perferendis temporibus</div>
+              <div>Description : Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, iure deleniti dolorem fuga praesentium perspiciatis adipisci aliquam ipsa saepe laboriosam.</div>
+            </OverlayText>
+          </WorkWrapper>
+
+          <WorkWrapper>
+            <img src="https://via.placeholder.com/300x300" alt=""/>
+            <OverlayText>
+              <div>Title</div>
+              <div>Description : Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, iure deleniti dolorem fuga praesentium perspiciatis adipisci aliquam ipsa saepe laboriosam.</div>
             </OverlayText>
           </WorkWrapper>
         </WorksContainer>
