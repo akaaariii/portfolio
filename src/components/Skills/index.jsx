6c67985acd5271aa8f32b667b2baf6e7fbe7e44e
akaaariii <caaari23@gmail.com>
2021-03-15 04:30:29 -0700

    added skill data

--- src/components/Skills/index.jsx
+++ src/components/Skills/index.jsx
@@ -1,85 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { BgSecondary, Wrapper } from '../../globalStyles'
-import { SkillsContainer } from './styles'
-import { Html5 } from '@styled-icons/simple-icons/Html5'
-import { Css3 } from '@styled-icons/simple-icons/Css3'
-import { Sass } from '@styled-icons/fa-brands/Sass'
-import { Javascript } from '@styled-icons/simple-icons/Javascript'
-import { Typescript } from '@styled-icons/simple-icons/Typescript'
-import { ReactLogo } from '@styled-icons/simple-icons/ReactLogo'
-import { Redux } from '@styled-icons/simple-icons/Redux'
-import { NodeDotJs } from '@styled-icons/simple-icons/NodeDotJs'
-import { Mongodb } from '@styled-icons/simple-icons/Mongodb'
-import { Firebase } from '@styled-icons/simple-icons/Firebase'
-import { Figma } from '@styled-icons/simple-icons/Figma'
-import { Git } from '@styled-icons/simple-icons/Git'
-import { TabletMobileCombo } from '@styled-icons/entypo/TabletMobileCombo'
-import { Eye } from '@styled-icons/fa-regular/Eye'
-// import { EmojiSmile } from '@styled-icons/bootstrap/EmojiSmile'
+import { SkillsContainer, SkillIcon, SkillText } from './styles'
+import { data } from './skillData'
 
 const Skills = () => {
+  // const [hover, setHover] = useState(false)
+  // const onHover = () => {
+  //   setHover(!hover)
+  // }
+
   return (
     <BgSecondary>
       <Wrapper id="skills">
         <h2>Skills</h2>
         <SkillsContainer>
-          <div>
-            <Html5 />
-            HTML
-          </div>
-          <div>
-            <Css3 />
-            CSS
-          </div>
-          <div>
-            <Sass />
-            Sass
-          </div>
-          <div>
-            <Javascript />
-            JavaScript
-          </div>
-          <div>
-            <Typescript />
-            TypeScript
-          </div>
-          <div>
-            <ReactLogo />
-            React
-          </div>
-          <div>
-            <Redux />
-            Redux
-          </div>
-          <div>
-            <NodeDotJs />
-            Node.js
-          </div>
-          <div>
-            <Mongodb />
-            MongoDB
-          </div>
-          <div>
-            <Firebase />
-            Firebase
-          </div>
-          <div>
-            <Figma />
-            Figma
-          </div>
-          <div>
-            <Git />
-            Git
-          </div>
-          <div>
-            <TabletMobileCombo />
-            Responsive Design
-          </div>
-          <div>
-            <Eye />
-            Keen eye for detail
-          </div>
-          {/* <EmojiSmile /> */}
+          {data.map((item, index) => (
+            <div key={index}>
+              <SkillIcon ><item.icon/></SkillIcon>
+              <SkillText>{item.tag}</SkillText>
+            </div>
+          ))}
         </SkillsContainer>
       </Wrapper>
     </BgSecondary>
---
+++ src/components/Skills/skillData.js
@@ -0,0 +1,87 @@
+import { Html5 } from '@styled-icons/simple-icons/Html5'
+import { Css3 } from '@styled-icons/simple-icons/Css3'
+import { Sass } from '@styled-icons/fa-brands/Sass'
+import { Javascript } from '@styled-icons/simple-icons/Javascript'
+import { Typescript } from '@styled-icons/simple-icons/Typescript'
+import { ReactLogo } from '@styled-icons/simple-icons/ReactLogo'
+import { Redux } from '@styled-icons/simple-icons/Redux'
+import { NodeDotJs } from '@styled-icons/simple-icons/NodeDotJs'
+import { Mongodb } from '@styled-icons/simple-icons/Mongodb'
+import { Firebase } from '@styled-icons/simple-icons/Firebase'
+import { Figma } from '@styled-icons/simple-icons/Figma'
+import { Git } from '@styled-icons/simple-icons/Git'
+import { TabletMobileCombo } from '@styled-icons/entypo/TabletMobileCombo'
+import { Eye } from '@styled-icons/fa-regular/Eye'
+
+export const data = [
+  {
+    "icon": Html5,
+    "tag": "HTML",
+    "color": "#E64C20",
+  },
+  {
+    "icon": Css3,
+    "tag": "CSS",
+    "color": "#254CDD",
+  },
+  {
+    "icon": Sass,
+    "tag": "Sass",
+    "color": "#C66495",
+  },
+  {
+    "icon": Javascript,
+    "tag": "JavaScript",
+    "color": "#EFD81A",
+  },
+  {
+    "icon": Typescript,
+    "tag": "TypeScript",
+    "color": "#3073BF",
+  },
+  {
+    "icon": ReactLogo,
+    "tag": "React",
+    "color": "#5ED3F2",
+  },
+  {
+    "icon": Redux,
+    "tag": "Redux",
+    "color": "#7248B6",
+  },
+  {
+    "icon": NodeDotJs,
+    "tag": "Node.js",
+    "color": "#519942",
+  },
+  {
+    "icon": Mongodb,
+    "tag": "MongoDB",
+    "color": "#11AA50",
+  },
+  {
+    "icon": Firebase,
+    "tag": "Firebase",
+    "color": "#FFCC2E",
+  },
+  {
+    "icon": Figma,
+    "tag": "Figma",
+    "color": "#9D57F6",
+  },
+  {
+    "icon": Git,
+    "tag": "Git",
+    "color": "#E94E31",
+  },
+  {
+    "icon": TabletMobileCombo,
+    "tag": "Responsive Design",
+    "color": "",
+  },
+  {
+    "icon": Eye,
+    "tag": "Keen eye for detail",
+    "color": "",
+  }
+]
