9286eda90665d18ba876aa80a46687914ca72bf7
akaaariii <caaari23@gmail.com>
2021-03-19 21:37:35 -0700

    added skill icon

--- src/components/About/styles.jsx
+++ src/components/About/styles.jsx
@@ -5,7 +5,7 @@ export const AboutContainer = styled.div`
   /* color: #4e4d4a; */
   padding: 36px;
 
-  @media (min-width: 600px){
+  @media (min-width: 576px){
     display: flex;
     flex-direction: row-reverse;
     justify-content: space-evenly;
--- src/components/Skills/index.jsx
+++ src/components/Skills/index.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { BgSecondary, Wrapper } from '../../globalStyles'
-import { SkillsContainer, SkillIcon, SkillText } from './styles'
+import { SkillsContainer, SkillIconContainer, SkillIcon, SkillText } from './styles'
 import { SkillGroup, SkillGroupTitle } from './styles'
 import { data } from './skillData'
 
@@ -15,25 +15,36 @@ const Skills = () => {
       <Wrapper id="skills">
         <h2>Skills</h2>
         <SkillsContainer>
-          <SkillGroup>
-            <SkillGroupTitle>Languages</SkillGroupTitle>
-            <div>
-              HTML, CSS, Sass, JavaScript, TypeScript
-            </div>
-          </SkillGroup>
-          <SkillGroup>
-            <SkillGroupTitle>Frameworks &amp; Libraries</SkillGroupTitle>
-            <div>
-              React, Redux, Chart.js, jQuery, Node.js, Express.js
-            </div>
-          </SkillGroup>
-          <SkillGroup>
-            <SkillGroupTitle>Others</SkillGroupTitle>
-            <div>
-              MongoDB, MySQL, Firebase, Git, Heroku, Figma, XD, Responsive Design
-            </div>
-          </SkillGroup>
+          <section>
+            <SkillGroup>
+              <SkillGroupTitle>Languages</SkillGroupTitle>
+              <div>
+                HTML, CSS, Sass, JavaScript, TypeScript
+              </div>
+            </SkillGroup>
+            <SkillGroup>
+              <SkillGroupTitle>Frameworks &amp; Libraries</SkillGroupTitle>
+              <div>
+                React, Redux, Chart.js, jQuery, Node.js, Express.js
+              </div>
+            </SkillGroup>
+            <SkillGroup>
+              <SkillGroupTitle>Others</SkillGroupTitle>
+              <div>
+                MongoDB, MySQL, Firebase, Git, Heroku, Figma, XD, Responsive Design
+              </div>
+            </SkillGroup>
+          </section>
+          
+          <SkillIconContainer>
+            {data.map((item, index) => (
+              <div key={index}>
+                <item.icon />
+              </div>
+            ))}
+          </SkillIconContainer>
         </SkillsContainer>
+
       </Wrapper>
     </BgSecondary>
   )
