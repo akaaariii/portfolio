1356aacdb838894acf5056025d5bfaaeaf8e05bf
akaaariii <caaari23@gmail.com>
2021-03-26 02:49:00 -0700

    Home: added image

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,16 +1,20 @@
 import React from 'react'
 import { BgBlack, Wrapper } from '../../globalStyles'
-import { HomeContainer, HeroSection, Name, FontEmph, MapleIcon } from './styles'
+import { HomeContainer, HeroSection, Name, FontEmph, ImageSection } from './styles'
+import flower from '../../assets/images/flower.gif'
 
 const Home = () => {
   return (
     <BgBlack>
       <Wrapper id="home">
         <HomeContainer>
+          <ImageSection>
+            <img src={flower} alt="flower"/>
+          </ImageSection>
           <HeroSection>
             I'm <Name>Akari</Name><br />
             <FontEmph>F</FontEmph>ront-<FontEmph>E</FontEmph>nd <FontEmph>D</FontEmph>eveloper,<br />
-            Based in <FontEmph>V</FontEmph>ancouver<MapleIcon />
+            Based in <FontEmph>V</FontEmph>ancouver
           </HeroSection>
         </HomeContainer>
       </Wrapper>
--- src/components/Home/styles.jsx
+++ src/components/Home/styles.jsx
@@ -1,10 +1,15 @@
 import styled from 'styled-components'
-import { CanadianMapleLeaf } from '@styled-icons/fa-brands/CanadianMapleLeaf'
 
 export const HomeContainer = styled.div`
   height: 70vh;
   display: flex;
+  flex-direction: column;
+  justify-content: center;
   align-items: center;
+
+  @media (min-width: 768px){
+    flex-direction: row-reverse;
+  }
 `
 
 export const HeroSection = styled.h2`
@@ -14,12 +19,14 @@ export const HeroSection = styled.h2`
   letter-spacing: .2rem;
   font-size: 1.2rem;
   font-weight: lighter;
-  padding: 0 36px 36px;
-  margin: auto;
+  padding: 0 36px 62px;
 
   @media (min-width: 576px){
     letter-spacing: .6rem;
     font-size: 1.5rem;
+  }
+  @media (min-width: 768px) {
+    padding: 0 36px;
   }
   @media (min-width: 1024px){
     letter-spacing: .7rem;
@@ -53,12 +60,16 @@ export const FontEmph = styled.span`
   }
 `
 
-export const MapleIcon = styled(CanadianMapleLeaf)`
-  color: #EAE7DC;
-  width: 50px;
-  transition: all 0.5s 0s ease-in-out;
+export const ImageSection = styled.div`
+  width: 60%;
+  margin-bottom: -80px;
 
-  :hover {
-    color: #3cb371;
+  @media (min-width: 576px){
+    width: 50%;
+  }
+  @media (min-width: 768px){
+    width: 30%;
+    margin-left: -46px;
+    margin-bottom: 0;
   }
 `
